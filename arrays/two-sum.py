995c7059ffaa332f032f1ca19177a39128686f09
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2024-05-06 21:20:36 -0700

    Delete arrays/two-sum.py

--- arrays/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sumToFindMap = {}
-        for i in range(len(nums)):
-            sumToFind = target - nums[i]
-            # case where current nums[i] satisfies number to find, so return here
-            if nums[i] in sumToFindMap:
-                return [sumToFindMap[nums[i]], i]
-            sumToFindMap[sumToFind] = i
